94221b5a7df2da4a7adc5d98e7845af7a36f8a7d
Muralish <mcmuralishclint@outlook.com>
2024-04-14 22:27:15 +0530

    Add more attributes to schedule

--- model/schedule.go
+++ model/schedule.go
@@ -1,10 +1,22 @@
 package model
 
+import "time"
+
+type ExecutionType string
+
+const (
+	OneTime   ExecutionType = "OneTime"
+	Recurrent ExecutionType = "Recurrent"
+)
+
 type Schedule struct {
-	ID          string
-	Action      string
-	Schedule    string
-	Description string
-	Priority    int
-	Data        string
+	ID            string
+	Action        string
+	Schedule      string
+	Description   string
+	Priority      int
+	Data          string
+	ExecuteAt     *time.Time
+	CreatedAt     time.Time
+	ExecutionType ExecutionType
 }
